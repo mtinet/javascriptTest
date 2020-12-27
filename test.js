ea9bd78bc7d5fa11ecba9bb4cbdde698e70e786b
mtinet <mtinet@hanmail.net>
2020-12-27 11:29:42 +0900

    ㅇㅇ

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
 const $apple = document.getElementById('apple');
 $apple.style.color = 'red';
+const $banana = document.getElementById('banana');
+$banana.style.color = 'yellow';
+const $orange = document.getElementById('orange');
+$orange.style.color = 'orange';
